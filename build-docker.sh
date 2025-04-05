6d4691d78cec835a743f61a61c88a09255d46c4d
comerc <andrew.kachanov@gmail.com>
2025-04-05 09:36:50 +0300

    Try update file

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 set -e  # выход при любой ошибке
 set -o pipefail  # выход при ошибке в любой команде пайплайна
 
-# Существует ли уже образ tdlib?
+# Существует ли уже образ tdlib???
 if ! docker image inspect tdlib:latest &>/dev/null; then
   echo "Building TDLib base image..."
   docker build -t tdlib:latest -f ./Dockerfile .
